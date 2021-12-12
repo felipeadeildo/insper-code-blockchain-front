83659740799b8a3c563cb12c1d83616c06f268e5
Kcpf <fe22122002@gmail.com>
2021-12-12 10:30:28 -0300

    adicionando página do time 2021.2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis"
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter basename={process.env.PUBLIC_URL} >
-      <App />
+      <MoralisProvider appId={process.env.REACT_APP_APP_ID} serverUrl={process.env.REACT_APP_SERVER_URL}>
+        <App />
+      </MoralisProvider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
